19bd1667764591ecad678c2f804c20e18adb2452
User <User@10.0.0.119>
2022-06-01 17:24:35 -0400

    Student code week 2

--- src/cardtrickice1/CardTrickICE1.java
+++ src/cardtrickice1/CardTrickICE1.java
@@ -4,6 +4,8 @@
  * and open the template in the editor.
  */
 package cardtrickice1;
+import java.util.Random;
+import java.util.Scanner;
 
 /** step1 : generate 7 random cards and store in array - how
  * step 2: take any card input from user suit,number
@@ -18,16 +20,76 @@ public class CardTrickICE1 {
      */
     public static void main(String[] args) 
     {
-        Card[] magicHand = new Card[7]; //Array of object
+        Random rand = new Random();
+        Scanner inp = new Scanner(System.in);
+        
+        Card one = new Card();
+        Card two = new Card();
+        Card three = new Card();
+        Card four = new Card();
+        Card five = new Card();
+        Card six = new Card();
+        Card seven = new Card();
+        Card eight = new Card();
+  
+        
+        Card[] magicHand = {one, two, three, four, five, six, seven, eight}; //Array of object
+        eight.setValue(10);
+	eight.setSuits("diamonds");
+
+        
         for( int i=0;i<magicHand.length;i++)
-        {
-            Card c1 = new Card();
-            c1.setValue(2);//use a method to generate random *13
-            c1.setSuits("hearts");//random method suit 
+        { 
+          
+            magicHand[i].setValue(rand.nextInt(13)+1);//use a method to generate random *13
+            magicHand[i].setSuits(magicHand[i].SUITS[rand.nextInt(4)]);//random method suit
+            
+            /*System.out.println(magicHand[i].getSuits());
+            System.out.println(magicHand[i].getValue());*/
+
         }
+
         //step 2:take input 
+        System.out.println(
+                "Please choose a suit\n"
+                + "1: Diamonds\n"
+                + "2: Clubs\n"
+                + "3: Spades\n"
+                + "4: Hearts\n" 
+        );
+        int ansOne = inp.nextInt();
+        
+        System.out.println(
+                "Please choose a value (1 to 13)"
+        );
+        int ansTwo = inp.nextInt();
+        
+        Card user = new Card();
+        switch(ansOne){ //Set Suits
+            case 1:
+                user.setSuits("diamonds");
+                break;
+            case 2:
+                user.setSuits("clubs");
+                break;
+            case 3:
+                user.setSuits("Spades");
+                break;
+            case 4:
+                user.setSuits("hearts");
+                break;
+        }
+        user.setValue(ansTwo);//Set Value
         
         //step 3: match with array 
+            for(int i=0; i<magicHand.length; i++){
+                if((user.getValue() == magicHand[i].getValue()) && (user.getSuits() == magicHand[i].getSuits())){
+                    System.out.println("Your card is in the Magic Hand!!");
+                }
+                else
+                    System.out.println("Your card not match");
+            }
+        }
     }
-    
-}
+
+
